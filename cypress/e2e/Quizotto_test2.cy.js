23cbce7a991b54e1a994cb58308cb6d8eba439fd
FeelGood500 <MichMik120@gmail.com>
2024-05-23 19:27:05 +0200

    git 2

--- cypress/e2e/Quizotto_test2.cy.js
+++ cypress/e2e/Quizotto_test2.cy.js
@@ -1,24 +1,21 @@
 describe('Quizotto', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5173/') // Załóżmy, że Twoja aplikacja jest uruchomiona pod adresem głównym
+    cy.visit('http://localhost:5173/') 
   })
 
   it('Wyświetla pytanie i możliwe odpowiedzi', () => {
     cy.get('.question').should('be.visible')
-    cy.get('.option').should('have.length', 3) // Sprawdzanie czy są trzy możliwe odpowiedzi
+    cy.get('.option').should('have.length', 3) 
   })
 
   it('Przechodzi przez wszystkie pytania i wyświetla wynik', () => {
-    const answers = [1, 2, 2, 0, 1, 0, 1, 1, 2, 1] // Poprawne odpowiedzi
+    const answers = [1, 2, 2, 0, 1, 0, 1, 1, 2, 1] 
 
     answers.forEach((answer, index) => {
-      // Sprawdza, czy pytanie jest widoczne
       cy.get('.question').should('be.visible')
 
-      // Wybiera odpowiedź
       cy.get('.option').eq(answer).click()
 
-      // Kliknij przycisk "Następne pytanie" lub "Podsumowanie" na ostatnim pytaniu
       if (index < answers.length - 1) {
         cy.get('button').contains('Następne pytanie').click()
       } else {
@@ -26,24 +23,19 @@ describe('Quizotto', () => {
       }
     })
 
-    // Sprawdza, czy wyświetla się komunikat o ukończeniu quizu
     cy.get('h2').should('contain', 'Ukończyłeś quiz!')
 
-    // Sprawdza, czy wynik jest poprawny
     cy.get('p').should('contain', 'Twój wynik to: 0 / 10!')
   })
 
   it('Resetuje quiz', () => {
-    // Odpowiada na pierwsze pytanie
-    cy.get('.option').first().click() // Wybierz pierwszą opcję
-    cy.get('button').contains('Następne pytanie').click() // Kliknij przycisk "Następne pytanie"
+    cy.get('.option').first().click() 
+    cy.get('button').contains('Następne pytanie').click() 
 
-    // Kliknij przycisk reset
     cy.get('#reset button').click()
 
-    // Sprawdź, czy quiz został zresetowany
-    cy.get('.question').should('be.visible') // Pytanie powinno być widoczne
-    cy.get('.option input').should('not.be.checked') // Żadne opcje nie powinny być zaznaczone
-    cy.get('.score').should('contain', 'Wynik 0/10') // Wynik powinien być zresetowany
+    cy.get('.question').should('be.visible') 
+    cy.get('.option input').should('not.be.checked') 
+    cy.get('.score').should('contain', 'Wynik 0/10') 
   })
 })
--- cypress/e2e/Quizotto_test3.cy.js
+++ cypress/e2e/Quizotto_test3.cy.js
@@ -1,37 +1,32 @@
 describe('Quizotto', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:5173/') // Załóżmy, że Twoja aplikacja jest uruchomiona pod adresem głównym
+      cy.visit('http://localhost:5173/') 
     })
   
     it('Wyświetla pytanie i możliwe odpowiedzi', () => {
       cy.get('.question').should('be.visible')
-      cy.get('.option').should('have.length', 3) // Sprawdzanie czy są trzy możliwe odpowiedzi
+      cy.get('.option').should('have.length', 3) 
     })
   
     it('Przechodzi przez 5 pytań', () => {
       const answers = [1, 2, 2, 0, 1]
   
       answers.forEach((answer, index) => {
-        // Sprawdza, czy pytanie jest widoczne
         cy.get('.question').should('be.visible')
   
-        // Wybiera odpowiedź
         cy.get('.option').eq(answer).click()
         })
     
     })
   
     it('Resetuje quiz', () => {
-      // Odpowiada na pierwsze pytanie
-      cy.get('.option').first().click() // Wybierz pierwszą opcję
-      cy.get('button').contains('Następne pytanie').click() // Kliknij przycisk "Następne pytanie"
+      cy.get('.option').first().click() 
+      cy.get('button').contains('Następne pytanie').click() 
   
-      // Kliknij przycisk reset
       cy.get('#reset button').click()
   
-      // Sprawdź, czy quiz został zresetowany
-      cy.get('.question').should('be.visible') // Pytanie powinno być widoczne
-      cy.get('.option input').should('not.be.checked') // Żadne opcje nie powinny być zaznaczone
-      cy.get('.score').should('contain', 'Wynik 0/10') // Wynik powinien być zresetowany
+      cy.get('.question').should('be.visible') 
+      cy.get('.option input').should('not.be.checked') 
+      cy.get('.score').should('contain', 'Wynik 0/10') 
     })
   })
